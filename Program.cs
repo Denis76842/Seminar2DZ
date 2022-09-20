829369ae1bd312f2c1a0d6cc3a54eca0a6f32af0
Denis76842 <112509983+Denis76842@users.noreply.github.com>
2022-09-20 14:05:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введи цифру дня недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("(выходной) -> да");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("Совсем не то");
+  }
+  else Console.WriteLine("( не выходной) -> нет");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
